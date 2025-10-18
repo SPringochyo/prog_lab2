50e6dc5cedbd507f5705e0c806d5a3018b545595
SPring <over.spring121@gmail.com>
2025-10-18 04:29:29 +0300

    sleeply changes

---
+++ src/moves/Swagger.java
@@ -0,0 +1,6 @@
+package moves;
+
+import pokemons.*;
+import ru.ifmo.se.pokemon.*;
+
+public class Swagger extends SpecialMove {}
---
+++ src/pokemons/Minun.java
@@ -0,0 +1,13 @@
+package pokemons;
+
+import moves.*;
+import ru.ifmo.se.pokemon.*;
+
+public class Minun extends Pokemon {
+
+    public Minun(String name, int level) {
+        super(name, level);
+        setType(Type.ELECTRIC);
+        setStats(60, 40, 50, 75, 85, 95);
+    }
+}
